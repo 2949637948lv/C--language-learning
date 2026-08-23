f86b6eba8ca3ca1974aeb0994cb0898aac6852f2
Orin_fine <2949637948lv@gmail.com>
2026-08-23 15:50:03 +0800

    Day 01:Add self-introduction program

---
+++ Day 01/Self-introduction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello,I'm Orin_fine\n"
+	       "and also a freshman \n"
+	       "majoring in Computer Science and Technology at Hunan Agricultural University");
+	
+	return 0;
+}
