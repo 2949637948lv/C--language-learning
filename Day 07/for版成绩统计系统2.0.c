6b3d0386e6a46a43a3ea211e7011f5379e7b93a2
Orin_fine <2949637948lv@gmail.com>
2026-08-30 21:05:43 +0800

    improve

---
+++ Day 07/for版成绩统计系统2.0.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+int main()
+{
+	int n=0;//总人数
+	float score=0;//得分
+	int great=0;//优秀人数
+	float sum=0.0;//总分
+	float max=0.0;
+	float min=0.0;
+	int a=0;//及格人数
+	int b=0;//不及格人数
+	int i=1;
+	
+	//100分制成绩
+	
+	printf("请输入班级总人数:\n");
+	scanf("%d",&n);
+	
+	printf("第1个学生成绩：\n");
+	scanf("%f",&score);
+	if(0<=score&&score<=100){
+	i++;
+	max=score;
+	min=score;
+		sum+=score;
+		if(score>=90){
+			great++;
+			a++;
+		}else if(score>=60){
+			a++;
+		}else{
+			b++;
+		}
+	}else{
+		printf("成绩无效！\n");
+	}
+	
+	for(i=2;i<=n;){
+		printf("第%d个学生成绩:\n",i);
+		scanf("%f",&score);
+			if(0<=score&&score<=100){
+				i++;
+		sum+=score;
+		if(score>max){
+			max=score;
+		}
+		if(score<min){
+			min=score;
+		}
+		if(score>=90){
+			great++;
+			a++;
+		}else if(score>=60){
+			a++;
+		}else{
+			b++;
+		}
+		}else{
+			printf("成绩无效！请重新输入！\n");
+		}
+	}
+	printf("总人数：%d\n",n);
+	printf("总分:%.2f\n",sum);
+	printf("平均分：%.2f\n",sum/n);
+	printf("最高分：%.2f\n",max);
+	printf("最低分：%.2f\n",min);
+	printf("及格人数：%d\n",a);
+	printf("不及格人数:%d\n",b);
+	printf("优秀人数：%d\n",great);	
+	
+	return 0;
+}
